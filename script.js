fffcb2ae032093565e04d05cf955bc7b607a8484
Lake Hills Media <lakehillsmedia@dhcp-172-22-21-146.us.kyndrylmobiledemo.net>
2024-07-05 22:36:09 -0400

    correct number logic added

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 'use strict';
 
 
-console.log(document.querySelector('.message'));
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = 'Correct Number!'
+
+// console.log(document.querySelector('.message').textContent);
+
+
+const secretNumber = Math.trunc(Math.random()*20)+1;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+
+    if (!guess) {
+        document.querySelector('.message').textContent = 'No Number!';
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = 'Correct Number!'
+    }
+});
+
