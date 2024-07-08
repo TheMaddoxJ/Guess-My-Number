e664473a05c876243e0abe55cc0e63369c833479
Lake Hills Media <lakehillsmedia@dhcp-172-22-21-146.us.kyndrylmobiledemo.net>
2024-07-08 03:49:41 -0400

    game over logic added

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
 'use strict';
 
-
-// console.log(document.querySelector('.message').textContent);
-
-// document.querySelector('.message').textContent = 'Correct Number!'
-
-// console.log(document.querySelector('.message').textContent);
-
-
 const secretNumber = Math.trunc(Math.random()*20)+1;
+let score = 20;
 document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
 
+
+    // Message Hint Logic and Score Updater Logic
     if (!guess) {
         document.querySelector('.message').textContent = '❌ No Number!';
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = '✅ Correct Number!';
     } else if (guess > secretNumber) {
-        document.querySelector('.message').textContent = '📈 Too High!';
+        if(score > 1) {
+            document.querySelector('.message').textContent = '📈 Too High!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = '👾 Game Over!';
+            document.querySelector('.score').textContent = 0;
+        }
+       
     } else if (guess < secretNumber) {
-        document.querySelector('.message').textContent = '📉 Too Low!'
+        if(score > 1) {
+            document.querySelector('.message').textContent = '📈 Too High!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = '👾 Game Over!';
+            document.querySelector('.score').textContent = 0;
+        }
     }
 });
 
